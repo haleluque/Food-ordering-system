6dee52feeb1117901570d0c4456af23add077292
haleluque <henry-luque@hotmail.com>
2024-03-05 19:03:39 -0500

    Solved errors regarding kafka publishing

--- restaurant-service/restaurant-messaging/src/main/java/com/food/ordering/system/restaurant/service/messaging/listener/kafka/RestaurantApprovalRequestKafkaListener.java
+++ restaurant-service/restaurant-messaging/src/main/java/com/food/ordering/system/restaurant/service/messaging/listener/kafka/RestaurantApprovalRequestKafkaListener.java
@@ -38,7 +38,7 @@ public class RestaurantApprovalRequestKafkaListener implements KafkaConsumer<Res
             topics = "${restaurant-service.restaurant-approval-request-topic-name}")
     public void receive(@Payload List<RestaurantApprovalRequestAvroModel> messages,
                         @Header(KafkaHeaders.RECEIVED_KEY) List<String> keys,
-                        @Header(KafkaHeaders.PARTITION) List<Integer> partitions,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION) List<Integer> partitions,
                         @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
         log.info("{} number of orders approval requests received with keys {}, partitions {} and offsets {}" +
                         ", sending for restaurant approval",
